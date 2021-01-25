39f02e6c11bb9a704252d796b1fcf2e7b5a0fa86
dunnohowtoplay <fauzanpartager@gmail.com>
2021-01-25 14:35:58 +0700

    change BASE_DIR

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -18,7 +18,7 @@ import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 dotenv_file = os.path.join(BASE_DIR, ".venv")
 if os.path.isfile(dotenv_file):
